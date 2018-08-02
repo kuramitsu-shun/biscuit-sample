9d20e4dbb28f2ab0c8a3b118f2d17d34a179357b
藏光　駿 <kuramitsu@kurahikarishunnoMacBook-Air-2.local>
2018-08-02 18:13:49 +0900

    2018080203

--- js/index.js
+++ js/index.js
@@ -1,24 +1,16 @@
-
-const elDebug = document.querySelector('#debug');
-
-window.addEventListener(
-  'deviceorientation',
-  deviceOrientationHandler);
-
-/** デバイスが傾いたときのイベントです。 */
-function deviceOrientationHandler(event) {
-  //ジャイロセンサー情報取得
-  // X軸
-  const beta = event.beta;
-  // Y軸
-  const gamma = event.gamma;
-  // Z軸
-  const alpha = event.alpha;
-
-  // ログを出力する
-  let html = ``;
-  html += 'X回転 : ' + beta + '<br />';
-  html += 'Y回転 : ' + gamma + '<br />';
-  html += 'Z回転 : ' + alpha;
-  elDebug.innerHTML = html;
-}
+window.onload = function(){
+    // デバイスの向きを取得
+    window.addEventListener("deviceorientation", function(e) {
+        e.preventDefault();
+        document.getElementById("oinfo").innerHTML = "alpha: " + parseInt(e.alpha) + " beta: " + parseInt(e.beta) + " gamma: " + parseInt(e.gamma);
+    }, true);
+    // デバイスの加速度、重力加速度、回転速度を取得
+    window.addEventListener("devicemotion", function(e) {
+        var a = e.acceleration;
+        var g = e.accelerationIncludingGravity;
+        var r = e.rotationRate;
+        document.getElementById("ainfo").innerHTML = "X: " + a.x.toFixed(3) + " Y: " + a.y.toFixed(3) + " Z: " + a.z.toFixed(3);
+        document.getElementById("ginfo").innerHTML = "X: " + g.x.toFixed(3) + " Y: " + g.y.toFixed(3) + " Z: " + g.z.toFixed(3);
+        document.getElementById("rinfo").innerHTML = "alpha: " + r.alpha.toFixed(3) + " beta: " + r.beta.toFixed(3) + " gamma: " + r.gamma.toFixed(3);
+    }, true);
+};
